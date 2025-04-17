f736397673cd07156f9c4c01f8ca9c9be6e3c265
Jared Hoberock <jaredhoberock@gmail.com>
2025-04-17 18:46:16 -0500

    Simplify build.rs and use parallel make

--- rust/build.rs
+++ rust/build.rs
@@ -4,20 +4,18 @@ use std::process::Command;
 fn main() {
     let dialect_dir = Path::new("..");
 
-    // build the C++ plugin
+    // Always run `make`. It handles incremental rebuilds itself.
     let status = Command::new("make")
+        .arg("-j")
         .current_dir(dialect_dir)
         .status()
-        .expect("Failed to build coord dialect C++");
+        .expect("Failed to run make");
 
     if !status.success() {
         panic!("C++ build failed");
     }
 
-    // link against the shared library
+    // Link against the shared library
     println!("cargo:rustc-link-search=native={}", dialect_dir.display());
     println!("cargo:rustc-link-lib=dylib=coord_dialect");
-
-    // trigger rebuild if any files in the dialect directory change
-    println!("cargo:rerun-if-changed={}", dialect_dir.display());
 }
